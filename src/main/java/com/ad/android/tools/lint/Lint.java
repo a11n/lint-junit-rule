8bda32ade0c0f38cb355f2595edbb02dd8975a35
a11n <andre.diermann@gmail.com>
2015-08-11 17:02:29 +0200

    Updated Lint junit rule (now using official lint-tests library under the hood)

--- src/main/java/com/ad/android/tools/lint/Lint.java
+++ src/main/java/com/ad/android/tools/lint/Lint.java
@@ -2,7 +2,11 @@ package com.ad.android.tools.lint;
 
 import com.android.annotations.NonNull;
 import com.android.tools.lint.Warning;
+import com.android.tools.lint.checks.infrastructure.LintDetectorTest;
+import com.android.tools.lint.detector.api.Detector;
 import com.android.tools.lint.detector.api.Issue;
+import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
@@ -29,16 +33,15 @@ import org.junit.runners.model.Statement;
  * </pre>
  */
 public class Lint implements TestRule {
-  TestLintClient testLintClient = new TestLintClient();
-
   private String[] files;
+  private Detector detector;
   private Issue[] issues;
 
   private List<Warning> warnings;
 
   /**
-   * Constructs a new {@code Lint} rule. <p> <b>Note:</b> Make sure to set files
-   * and issues before calling {@code analyze()}.
+   * Constructs a new {@code Lint} rule. <p> <b>Note:</b> Make sure to set files, detector and
+   * issues before calling {@code analyze()}.
    */
   public Lint() {
   }
@@ -46,24 +49,25 @@ public class Lint implements TestRule {
   /**
    * Constructs a new {@code Lint} rule.
    *
-   * @param files to be considered by Lint analysis.
-   * @param issues to be considered by Lint analysis.
+   * @param files to be analyzed by Lint.
+   * @param detector to use for Lint analysis.
+   * @param issues to be reported by Lint analysis.
    */
-  public Lint(@NonNull String[] files, @NonNull Issue[] issues) {
+  public Lint(@NonNull String[] files, @NonNull Detector detector, @NonNull Issue[] issues) {
     this.files = files;
+    this.detector = detector;
     this.issues = issues;
   }
 
   /**
-   * This is called by the JUnit runner. <p> You don't need to interact with
-   * this method.
+   * This is called by the JUnit runner. <p> You don't need to interact with this method.
    */
   @Override public Statement apply(Statement base, Description description) {
     return new LintStatement(base);
   }
 
   /**
-   * Set files to be considered by Lint analysis.
+   * Set files to be analyzed by Lint.
    *
    * @param relativePaths to the file, starting at test/resources.
    */
@@ -72,37 +76,49 @@ public class Lint implements TestRule {
   }
 
   /**
-   * Set issues to be considered by Lint analysis.
+   * Set detector to use for Lint analysis.
    *
-   * @param issues to be considered by Lint analysis.
+   * @param detector to use for Lint analysis.
+   */
+  public void setDetector(@NonNull Detector detector) {
+    this.detector = detector;
+  }
+
+  /**
+   * Set issues to be reported by Lint analysis.
+   *
+   * @param issues to be reported by Lint analysis.
    */
   public void setIssues(@NonNull Issue... issues) {
     this.issues = issues;
   }
 
   /**
-   * Performs a Lint analysis on the specified files and searching for the
-   * specified issues. <p> Populated warnings could be obtained with {@code
-   * getWarnings()}. <p> <b>Note:</b> Make sure {@code files} and {@code issues}
-   * have been set prior to calling {@code analyze()}. Otherwise an {@code
-   * IllegalStateException} will be thrown.
+   * Performs a Lint analysis on the specified files, using the specified detector and searching for
+   * the specified issues. <p> Populated warnings could be obtained with {@code getWarnings()}. <p>
+   * <b>Note:</b> Make sure {@code files}, {@code detector} and {@code issues} have been set prior
+   * to calling {@code analyze()}. Otherwise an {@code IllegalStateException} will be thrown.
    *
-   * @throws Exception for unexpected behavior during Lint analysis (e.g. could
-   * not read or access file)
+   * @throws Exception for unexpected behavior during Lint analysis (e.g. could not read or access
+   * file)
    */
   public void analyze() throws Exception {
-    if (files == null || issues == null) {
-      throw new IllegalStateException("Files or issues must not be null");
+    if (files == null || detector == null || issues == null) {
+      throw new IllegalStateException("Files, detector or issues must not be null");
     }
 
-    warnings = testLintClient.analyze(files, issues);
+    GenericLintDetectorTest genericLintDetectorTest = new GenericLintDetectorTest(detector,
+        Arrays.asList(issues));
+
+    genericLintDetectorTest.analyze(files);
+
+    warnings = genericLintDetectorTest.getWarnings();
   }
 
   /**
-   * Gets a list of warnings, populated by a previous Lint analysis. <p>A
-   * successful Lint analysis will result in an empty (size == 0) list of
-   * warnings.<p> <b>Note:</b> Calling this prior to calling {@code analyze()}
-   * will result in an {@code IllegalStateException}.
+   * Gets a list of warnings, populated by a previous Lint analysis. <p>A successful Lint analysis
+   * will result in an empty (size == 0) list of warnings.<p> <b>Note:</b> Calling this prior to
+   * calling {@code analyze()} will result in an {@code IllegalStateException}.
    *
    * @return a list of warnings
    */
@@ -123,6 +139,51 @@ public class Lint implements TestRule {
 
     @Override public void evaluate() throws Throwable {
       base.evaluate();
+    }
+  }
+
+  private class GenericLintDetectorTest extends LintDetectorTest {
+
+    private final Detector detector;
+    private final List<Issue> issues;
+
+    private List<Warning> warnings;
+
+    public GenericLintDetectorTest(Detector detector, List<Issue> issues) {
+      this.detector = detector;
+      this.issues = issues;
+    }
+
+    public void analyze(String... files) throws Exception {
+      lintFiles(files);
+    }
+
+    public List<Warning> getWarnings() {
+      return warnings;
+    }
+
+    @Override protected Detector getDetector() {
+      return detector;
+    }
+
+    @Override protected List<Issue> getIssues() {
+      return issues;
+    }
+
+    @Override protected String checkLint(List<File> files) throws Exception {
+      ExtendedTestLintClient testLintClient = new ExtendedTestLintClient();
+      String result = checkLint(testLintClient, files);
+
+      warnings = testLintClient.getWarning();
+
+      return result;
+    }
+
+    private class ExtendedTestLintClient extends TestLintClient {
+
+      public List<Warning> getWarning() {
+        return mWarnings;
+      }
     }
   }
 }
