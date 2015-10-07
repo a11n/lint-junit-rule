563bebff10c6faa2fbd078bee110e65b2f1c17a6
a11n <andre.diermann@gmail.com>
2015-10-07 13:23:34 +0200

    lint-junit-rule-3 Add sample

---
+++ sample/src/test/java/de/ad/android/tools/lint/HelloWorldDetectorTest.java
@@ -0,0 +1,37 @@
+package de.ad.android.tools.lint;
+
+import com.ad.android.tools.lint.Lint;
+import com.ad.android.tools.lint.assertj.WarningAssert;
+import com.android.tools.lint.Warning;
+import java.util.List;
+import org.hamcrest.MatcherAssert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static com.ad.android.tools.lint.hamcrest.WarningMatcher.atLine;
+import static com.ad.android.tools.lint.hamcrest.WarningMatcher.hasWarnings;
+import static com.ad.android.tools.lint.hamcrest.WarningMatcher.in;
+import static com.ad.android.tools.lint.hamcrest.WarningMatcher.withMessage;
+
+public class HelloWorldDetectorTest {
+  @Rule public Lint lint = new Lint(new HelloWorldDetector(), HelloWorldDetector.ISSUE);
+
+  @Test
+  public void shouldDetectNoWarning() throws Exception {
+    List<Warning> lintResult = lint.files("AndroidManifest.xml");
+
+    //AssertJ
+    WarningAssert.assertThat(lintResult)
+        .hasWarnings(1)
+        .in("AndroidManifest.xml")
+        .atLine(7)
+        .withMessage("Unexpected title \"@string/app_name\". Should be \"Hello world.\".");
+
+    //Hamcrest
+    MatcherAssert.assertThat(lintResult,
+        hasWarnings(
+            in("AndroidManifest.xml"),
+            atLine(7),
+            withMessage("Unexpected title \"@string/app_name\". Should be \"Hello world.\".")));
+  }
+}
